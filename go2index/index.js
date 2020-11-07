9d22f88c79c412cebd9d78b6998c7fdadf531cbd
thy7349 <63000448+thy7349@users.noreply.github.com>
2020-11-07 10:16:15 +0800

    Update index.js

--- go2index/index.js
+++ go2index/index.js
@@ -4,8 +4,8 @@ var authConfig = {
   version: "1.1.2", // 程序版本
   theme: "acrou",
   // 强烈推荐使用自己的 client_id 和 client_secret
-  client_id: "202264815644.apps.googleusercontent.com",
-  client_secret: "X4Z3ca8xfWDb1Voo-F9a7ZxJ",
+  client_id: "792248657427-9gosk18usknk2l41si8bfdnnt5psl7f7.apps.googleusercontent.com",
+  client_secret: "tIn_K3UICaaTi4VkTzUHGpPE",
   refresh_token: "", // 授权 token
   /**
    * 设置要显示的多个云端硬盘；按格式添加多个
